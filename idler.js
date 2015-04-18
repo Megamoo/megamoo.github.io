93f11c9dd3934fdc236a8695fc7463c04c0f99e8
Kacper Abucewicz <virgil851@live.com>
2015-04-18 17:41:22 -0500

    Test Update

--- idler.js
+++ idler.js
@@ -2,15 +2,31 @@
  * Created by Megamoo on 4/16/2015.
  */
 var money, income;
+//var Game = new NewGame();
 income = 1;
 money = 0;
+
+
 function Tick() {
     money = money + income;
     document.getElementById("money").innerHTML = money;
 }
+
 function GatherMoney() {
-    money = money + 1 + income;
+    money = money + income;
     document.getElementById("money").innerHTML = money;
 }
 
 var Timer = window.setInterval(function(){Tick()}, 1000);
+
+function Save() {
+    window.localStorage['SaveName'] = JSON.stringify(Game);
+}
+function Load () {
+    window.GameTwo = JSON.parse(window.localStorage['SaveName']);
+}
+
+function NewGame() {
+    this.money = [money];
+    this.income = [income];
+}
